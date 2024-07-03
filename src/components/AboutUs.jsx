d1159495efb06ea89443bba1b13b2556d76f52c6
Sammienator <sammugo18@gmail.com>
2024-07-03 23:52:41 +0300

    changes

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,8 +1,81 @@
 import React from 'react'
+import {MdHomeRepairService} from "react-icons/md"
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div className='w-11/12 m-auto flex flex-col md:flex-row justify justify-between items-center md:space-x-10 space-y-10 md:space-y-0 py-10  '>
+
+        <div className='flex space-x-4 md:space-x-0'>
+            <div className='mt-10 space-y-4 '>
+                <div className='w-48 flex flex-col items-center bg-white text-center
+                 p-5 drop-shadow-2xl rounded-md'>
+                    <MdHomeRepairService size={'1.8rem'} />
+                    <h1>Good Services </h1>
+                    <p> Lorem ipsum "We cannot trample upon the humanity of others without devaluing our own. The Igbo, always practical, put it concretely in their proverb: 'Onye ji onye n'ani ji onwe ya': 'He who will hold another down in the mud must stay in the mud to keep him down.'" </p>
+                </div>
+
+
+
+
+                <div className='w-48 flex flex-col items-center bg-white text-center
+                 p-5 drop-shadow-2xl rounded-md' >
+                    <MdHomeRepairService size={'1.8rem'} />
+                    <h1>Good Services </h1>
+                    <p> Lorem ipsum "We cannot trample upon the humanity of others without devaluing our own. The Igbo, always practical, put it concretely in their proverb: 'Onye ji onye n'ani ji onwe ya': 'He who will hold another down in the mud must stay in the mud to keep him down.'" </p>
+                </div>
+
+
+
+
+
+
+                
+            </div>
+            
+            <div>
+                <div className='w-48 flex flex-col items-center bg-white text-center
+                 p-5 drop-shadow-2xl rounded-md' >
+                    <MdHomeRepairService size={'1.8rem'} />
+                    <h1>Good Services </h1>
+                    <p> Lorem ipsum "We cannot trample upon the humanity of others without devaluing our own. The Igbo, always practical, put it concretely in their proverb: 'Onye ji onye n'ani ji onwe ya': 'He who will hold another down in the mud must stay in the mud to keep him down.'" </p>
+                </div>
+
+
+
+
+                <div className='w-48 flex flex-col items-center bg-white text-center
+                 p-5 drop-shadow-2xl rounded-md' >
+                    <MdHomeRepairService size={'1.8rem'} />
+                    <h1>Good Services </h1>
+                    <p> Lorem ipsum "We cannot trample upon the humanity of others without devaluing our own. The Igbo, always practical, put it concretely in their proverb: 'Onye ji onye n'ani ji onwe ya': 'He who will hold another down in the mud must stay in the mud to keep him down.'" </p>
+                </div>
+
+
+
+
+
+
+                
+            </div>
+            
+        </div>      
+
+
+        <div>
+            <h1> Know
+                <span> About Us </span>
+            </h1>
+            <p>
+            Lorem ipsum "We cannot trample upon the humanity of others without devaluing our own. The Igbo, always practical, put it concretely in their proverb: 'Onye ji onye n'ani ji onwe ya': 'He who will hold another down in the mud must stay in the mud to keep him down.'"
+            </p>
+            <button>
+                Read More
+            </button>
+            
+        </div>     
+
+
+    </div>
   )
 }
 
